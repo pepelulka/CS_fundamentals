7a5f487a14e05557563d92977bc769544b56d055
pepelulka <mrpepelulka@gmail.com>
2022-12-12 21:49:13 +0300

    x

--- Lab12-integers_in_C/a.c
+++ Lab12-integers_in_C/a.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void swap(char* string, int ind1, int ind2) {
     char temp = string[ind1];
@@ -6,22 +7,22 @@ void swap(char* string, int ind1, int ind2) {
     string[ind2] = temp;
 }
 
-void solve() {
-    char buffer[40];
+void input(char* buffer) {
     int ptr = 0;
     char cur;
-    
     cur = getchar();
     while (cur != '\n') {
         buffer[ptr] = cur; ptr++;
         cur = getchar();
     }
     buffer[ptr] = '\0';
-    swap(buffer, 0, ptr - 1);
-    printf(buffer);
 }
 
 int main() {
-    solve();
+    char buffer[12];
+    input(buffer);
+    if (buffer[0] != '-' && buffer[0] != '+') swap(buffer, 1, strlen(buffer) - 1);
+    else swap(buffer, 0, strlen(buffer) - 1);
+    printf(buffer);
     return 0;
 }  
